7f15fb0682be83f95b271a15d7ad139a8dbe2794
Fady <fedo.zaher@gmail.com>
2025-02-20 16:37:02 +0200

    enhance

--- lib/src/presentation/screens/portfolio/widgets/my_chapters_career_widget.dart
+++ lib/src/presentation/screens/portfolio/widgets/my_chapters_career_widget.dart
@@ -4,8 +4,7 @@ import 'package:my_portfolio/src/config/theme/color_schemes.dart';
 import 'package:my_portfolio/src/core/resources/image_paths.dart';
 import 'package:my_portfolio/src/domain/model/time_line.dart';
  import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/bounce_and_scale_text_widget.dart';
-import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/my_chapters_career_widget.dart';
-import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/time_line_career_widget.dart';
+ import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/time_line_career_widget.dart';
  import 'package:visibility_detector/visibility_detector.dart';
 
 class MyChaptersCareerWidget extends StatefulWidget {
