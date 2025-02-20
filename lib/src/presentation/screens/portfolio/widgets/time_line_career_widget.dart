86c9b32d754b2551e4ddba336a2b4f37876f95e2
Fady <Fady.zaher@spriteye.com>
2025-02-20 16:04:55 +0200

    my portfolio

--- lib/src/presentation/screens/portfolio/widgets/time_line_career_widget.dart
+++ lib/src/presentation/screens/portfolio/widgets/time_line_career_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/src/config/theme/color_schemes.dart';
 import 'package:my_portfolio/src/core/utils/openLink.dart';
+import 'package:my_portfolio/src/domain/model/time_line.dart';
 import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/animated_text_Widget.dart';
-import 'package:my_portfolio/src/presentation/screens/portfolio/widgets/my_chapters_career_widget.dart';
 
 class TimelineCareerWidget extends StatefulWidget {
   final TimelineEvent event;
